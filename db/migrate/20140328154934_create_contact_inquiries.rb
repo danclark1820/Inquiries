2f0062ae792b6da7139f13ee6c7bf42e0a79a041
danclark1820 <danclark1820@gmail.com>
2014-03-28 11:58:37 -0400

    Set up unit tests for Contact Inquiry

--- db/migrate/20140328154934_create_contact_inquiries.rb
+++ db/migrate/20140328154934_create_contact_inquiries.rb
@@ -3,9 +3,9 @@ class CreateContactInquiries < ActiveRecord::Migration
     create_table :contact_inquiries do |t|
       t.string :email,      null: false
       t.string :subject,    null: false
-      t.text :description,  null:false
-      t.string :first_name, null:false
-      t.string :last_name,  null:false
+      t.text :description,  null: false
+      t.string :first_name, null: false
+      t.string :last_name,  null: false
 
       t.timestamps
     end
--- spec/models/contact_inquiry_spec.rb
+++ spec/models/contact_inquiry_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe ContactInquiry do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has an email'
+  it 'has a subject'
+  it 'has a description'
+  it 'has a first name'
+  it 'has a last name'
 end
