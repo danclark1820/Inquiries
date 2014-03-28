5ee2dbbd3ba812fe39ec5e523fc5be743152ce04
danclark1820 <danclark1820@gmail.com>
2014-03-28 11:37:50 -0400

    creaetd application, set up testing enviorment

---
+++ spec/features/user_submits_contact_inquiry_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+feature "User submits contact inquiry", %q{
+  As a user
+  I want to create an issue
+  So that I can track the progress of the issue
+} do
+
+#Acceptance Criteria:
+# * I must specify a valid email address
+# * I must specify a subject
+# * I must specify a first name
+# * I must specify a last name
+
+  context "with valid attributes" do
+    it "creates an issue with valid attributes"
+  end
+
+  context "with invalid attributes" do
+    it "sees errors for invalid attributes"
+  end
+end
